7ca1bd52e90e0bbc08556ca18a4962ccc5a3c352
Christopher Vega <Christophervega@mail.weber.edu>
2022-01-19 23:03:11 -0700

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,1 +1,5 @@
-hi
+print("Hello")
+print("It was a dark and stormy night.")
+print("Suddenly a shot rang out!")
+print("Hi! This is a double quote mark: \" We use it for printing.")
+print("Forrest Gump once said \"I just stopped running. I didn't feel like running anymore!.\"")
