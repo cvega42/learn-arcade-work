d8a7486f44078251bdc0fa85cae4d1ddbd83b187
Christopher Vega <Christophervega@mail.weber.edu>
2022-01-23 08:28:15 -0700

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("My wife is helping me code, isn't that awesome?")
+print("This day has been really exhausting, I'm going to sleep good tonight.")
+print("I'm trying to sleep, but my dogs woke me up. Now I'm working on this.")
+print("Forrest Gump once said, \'I just stopped running. I didn't feel like running no more!.\'")
