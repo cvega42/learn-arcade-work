6a887abeffb25cf5aaa97869f76f0852e2109c33
Christopher Vega <Christophervega@mail.weber.edu>
2022-04-05 18:23:00 -0600

    Tried to commit assignment #7

--- Lab 08 - Sprites/lab_08.py
+++ Lab 08 - Sprites/lab_08.py
@@ -25,13 +25,10 @@ class MyGame(arcade.Window):
     """ Main application class """
 
 
+
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
 
-        # Set the working directory (where we expect to find files) to the same
-        # directory this .py file is in. You can leave this out of your own
-        # code, but it is needed to easily run the examples using "python -m"
-        # as mentioned at the top of this program.
         file_path = os.path.dirname(os.path.abspath(__file__))
         os.chdir(file_path)
 
